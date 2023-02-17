9827a7033b6c9d880073b07eb7aa999b15fdd782
gbc6292 <6292@holbertonstudents.com>
2023-02-17 21:17:20 +0000

    Adding puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - 
- * * return: The program needs to end in 0
+ * main - first entry
+ * Return: program need to end in 0
  */
 
-{int main(void)
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0)
-}
+int main(void)
+{
+	pits("\"Programing is like building a multilingual puzzle")
+	return (0)
