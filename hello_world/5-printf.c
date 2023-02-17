0837fef216f1991ce8f8f118453b48c2d05276e2
gbc6292 <6292@holbertonstudents.com>
2023-02-17 21:45:46 +0000

    Using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing without function puts
+ * Return: Your program should compile without warning when using the -Wall gcc option
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
