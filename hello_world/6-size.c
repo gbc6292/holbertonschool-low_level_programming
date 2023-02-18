cdd61583fd25d657d34f53c4effb51c7acac7b12
gbc6292 <6292@holbertonstudents.com>
2023-02-18 19:31:52 +0000

    Adding SizeType

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-	prinft("Size of char: %d bytes\n", sizef(charType));
-	prinft("Size of int: %d bytes\n", sizef(intType));
-	printf("Size of long int: %d bytes\n", sizef(longintType));
-	printf("Size of long long int: %d bytes\n", sizef(longlongintType));
-	prinft("Size of float: %d bytes\n", sizef(floatType));
+	prinft("Size of char: %d bytes\n", sizef(char Type));
+	prinft("Size of int: %d bytes\n", sizef(int Type));
+	printf("Size of long int: %d bytes\n", sizef(long int Type));
+	printf("Size of long long int: %d bytes\n", sizef(long long int Type));
+	prinft("Size of float: %d bytes\n", sizef(float Type));
 	return (0);
 }
