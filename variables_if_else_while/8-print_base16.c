2b9f97279c8c8b465669a86e6510127e7823cd55
gbc6292 <6292@holbertonstudents.com>
2023-02-22 04:04:03 +0000

    Adding Print Base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - Prinitng
+ *  Return: Always en in 0
+ *
+ */
+
+int main(void)
+{
+	int a;
+	char ch;
+
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+	for (ch = '0'; ch <= 'f'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
