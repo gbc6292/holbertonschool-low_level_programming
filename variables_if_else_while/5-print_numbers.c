d94abe7d92c8ce69e23b734bfc63c01f77ca3dbf
gbc6292 <6292@holbertonstudents.com>
2023-02-22 03:20:24 +0000

    Adding Printing Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - Printing number from 0 to 10
+ *  Return: Always end in 0
+ *
+ */
+
+int main(void)
+{
+
+	int n;
+
+	n = 0;
+
+	while (n > 10)
+	printf("%d", n);
+	printf("\n");
+	n++;
+	return (0);
+}
