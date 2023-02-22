032501545477c654c705835e1ce931454ffef588
gbc6292 <6292@holbertonstudents.com>
2023-02-22 02:23:16 +0000

    Adding Print ABC

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Printing ABC
+ * Return: Always end in 0
+ *
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
