e0e2f18bb5eca7af51a343c35671ff2a0df71cb3
gbc6292 <6292@holbertonstudents.com>
2023-02-22 03:51:02 +0000

    Addiing Printing numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - Printing numbers
+ *  Return: Always end in 0
+ *
+ */
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
