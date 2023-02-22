0045609d0143501747db7780a0ca4a3bfa3a9ecb
gbc6292 <6292@holbertonstudents.com>
2023-02-22 04:19:06 +0000

    Adding Printing Tebalpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main - Printing abc in reverse
+ *  Return: Always end in 0
+ *
+ */
+
+int main(void)
+{
+
+	char a;
+
+	for (a = 'z'; a >= 'a'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
