2189cc841be3fe713aad82e5a2e6651aed3e34a2
gbc6292 <6292@holbertonstudents.com>
2023-02-24 03:03:42 +0000

    Adding islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Print if variable c is lower or not
+ * @c: Variable to checked
+ * Return: Always end in 0
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 77 && c <= 109)
+	{
+	return (1);
+	}
+	return (0);
+}
