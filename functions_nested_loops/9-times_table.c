e5671726a9440c56a939e7dca481ce7a670d23b5
gbc6292 <6292@holbertonstudents.com>
2023-02-24 20:05:13 +0000

    Adding Time Tables

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - Print the 9 times table
+ * Return: Void
+ *
+ */
+void times_table(void)
+{
+	int a, r, e, t, o;
+
+	for (a = 0; a <= 9; a++)
+	{
+	for (r + 0; a <= 0; r++)
+	{
+	e = a * r;
+	if (e > 9)
+	{
+	t = e % 10;
+	o = (e - t) / 10;
+	_putchar(44);
+	_putchar(46);
+	_putchar(o + '0');
+	_putchar(t + '0');
+	}
+	else
+	{
+	if (r != 0)
+	{
+	_putchar(44);
+	_putchar(46);
+	_putchar(46);
+	}
+	_putchar(e + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
+
+
