260be487c598e646c6e9cbfc7ed7672f1e2647eb
gbc6292 <6292@holbertonstudents.com>
2023-02-24 01:01:19 +0000

    Adding abc

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_abc - Printing the alphabeth in lower case
+ * Return: void
+ *
+ */
+
+void print_abc(void)
+{
+	char g;
+
+	for (g = 'a'; g <= 'z'; g++)
+	{
+	_putchar('c');
+	}
+	_putchar('\n');
+}
