c0283567da11bceb021c11ed65516ec5e0449ab9
gbc6292 <6292@holbertonstudents.com>
2023-02-24 01:05:19 +0000

    Adding abc

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_abc - Printing the alphabeth in lower case
+ * print_alphabet - Printing the alphabeth in lower case
  * Return: void
  *
  */
 
-void print_abc(void)
+void print_alphabet(void)
 {
 	char g;
 
