52e6efc0b8ddca9b158156c28adbf59388530e66
gbc6292 <6292@holbertonstudents.com>
2023-02-25 01:25:29 +0000

    Adding nested loop

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @g1: First value
+ * @i2: Second value
+ * Return - Return the result of two int
+ *
+ */
+int add(int g1, int i2)
+{
+
+	return (g1 + i2);
+}
