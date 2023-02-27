e96645acac563436170bca2a58e6565fb0185790
gbc6292 <6292@holbertonstudents.com>
2023-02-27 17:32:13 +0000

    adding 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * more_numbers
+ * more_numbers - Printin 10 times numbers from 0 to 14
  * Return: Void
  *
  */
@@ -10,13 +10,13 @@ void more_numbers(void)
 
 	for (o = 0; o < 10; o++)
 	{
-	for (v = 0; c < 14; v++)
+	for (v = 0; v < 14; v++)
 	{
-	if (v > 9)
+	if (o > 9)
 	{
-	_putchar((y / 10) + '0');
+	_putchar((o / 10) + '0');
 	}
-	_putchar((y % 10) + '0');
+	_putchar((o % 10) + '0');
 	}
 
 }
