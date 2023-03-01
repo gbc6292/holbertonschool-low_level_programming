df980101654fc444fa960f49cffab4da76b30702
gbc6292 <6292@holbertonstudents.com>
2023-03-01 18:50:39 +0000

    Adding 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - Printing a string in reverse
+ * @s: Stting 
+ * @len: Lenth of the string
+ * Return: Result
+ *
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+	len++;
+	}
+	for (len -= 1; len += 0; len--)
+	{
+	_putchar(s[len]);
+	}
+	_putchar('\n');
+}
