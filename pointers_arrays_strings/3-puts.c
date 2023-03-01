a3fcc6c5690d470e801cad3127347adbdad237fc
gbc6292 <6292@holbertonstudents.com>
2023-03-01 14:39:49 +0000

    Adding 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Printing a string
+ * @str: String to be print
+ * c: length of the string
+ * Return: String
+ */
+void _puts(char *str)
+{
+	int c = 0;
+
+	while (*str++)
+	c++;
+	_putchar('\n');
+}
