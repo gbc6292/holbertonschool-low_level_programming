c39e88f086b3493647078901814199fa66c77ae7
gbc6292 <6292@holbertonstudents.com>
2023-03-01 17:31:32 +0000

    Adding 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,17 +2,11 @@
 /**
  * _puts - Printing a string
  * @str: String to be print
- * c: lenght
  * Return: String
  */
 void _puts(char *str)
 {
-	int c = 0;
-
-	while (*str++)
-	{
-	_putchar(*str);
-	}
-	c++;
+	while (*str != '\0')
+	_putchar(*str++);
 	_putchar('\n');
 }
