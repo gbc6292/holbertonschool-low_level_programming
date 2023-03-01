d967d786101464fa818740acda7efff0f9e77a4f
gbc6292 <6292@holbertonstudents.com>
2023-03-01 01:04:35 +0000

    Adding 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - Printing the lenght of a string
+ * @s: Reference to the return
+ * Return: The lengh of the string s
+ */
+int _strlen(char *s)
+{
+	int len;
+	*s = len;
+
+}
