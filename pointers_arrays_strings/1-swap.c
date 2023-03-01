e295c76e5af6968a2e6c0c9df5e9084d0a94ffd6
gbc6292 <6292@holbertonstudents.com>
2023-03-01 00:33:21 +0000

    Adding 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - switching the values of two intergers
+ * @a: First pointer to be switch
+ * @b: Second pointer to be switch
+ * Return - N/a
+ *
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+
+}
