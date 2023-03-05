8b05df19b3cc0290fcf52ec47fb989448ce88f75
gbc6292 <6292@holbertonstudents.com>
2023-03-05 00:59:21 +0000

    Adding 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,8 @@ void rev_string(char *s)
 	int a;
 
 	for (a = 0; s[a] != '\0'; a++)
-	{
 	_putchar(s[a]);
-	}
 	_putchar('\n');
-	for (a -= 1; a >= 0; a--);
+	for (a -= 1; a >= 0; a--)
+	_putchar(s[a];
 }
