c302bf2bb8161eba6febb121db8f3244afb48ba1
gbc6292 <6292@holbertonstudents.com>
2023-03-05 01:55:13 +0000

    Adding 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - printing other characters of the string
+ * @str: The string of reference
+ * Return: void
+ *
+ */
+
+void puts3(char *str)
+{
+	int a;
+	int b = 0;
+
+	while (str[b] != '\0')
+	{
+	j++;
+	}
+
+	for (a = 0; 1 < b; a += 2)
+	{
+	_putchar(str[a]);
+	}
+	_putchar('\n');
+}
