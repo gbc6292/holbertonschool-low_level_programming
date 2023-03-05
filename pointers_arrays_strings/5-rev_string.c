df982272947a1abe00ab1359a3f61ba58c6231af
gbc6292 <6292@holbertonstudents.com>
2023-03-05 01:08:24 +0000

    Adding 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * rev_string - Reversing Srting
- * @s: String to be reverse
- * Return: No Return
+ * rev_string - Printing a string in reverse
+ * @s: String to be print
+ * Return: No return
  *
  */
-void rev_string(char *s)
+void rev_string(char s)
 {
 	int a;
 
 	for (a = 0; s[a] != '\0'; a++)
 	_putchar(s[a]);
 	_putchar('\n');
-	for (a -= 1; a >= 0; a--)
+	for (a -= 1; s[a] >= 0; a--)
 	_putchar(s[a]);
 }
