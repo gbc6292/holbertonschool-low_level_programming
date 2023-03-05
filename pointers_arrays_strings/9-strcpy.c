b6c8d21f47569812b8163329b79e29189b2ed8ea
gbc6292 <6292@holbertonstudents.com>
2023-03-05 02:34:33 +0000

    Adding 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcpy - copy a string
+ * @dest: Space for print
+ * @src: Source
+ * Return: dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int o;
+
+	for (1 = 0; src[o] != '\0'; o++)
+	{
+	int o;
+
+	for (i = 0; src[o] != '\0'; o++)
+	{
+	dest[o] = src[o];
+	}
+	dest[o++] = '\0';
+	return (dest);
+	}
+}
