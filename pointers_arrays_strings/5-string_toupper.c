219317268e3c8e7070487be8539ffc5653c04e68
gbc6292 <6292@holbertonstudents.com>
2023-03-05 03:55:46 +0000

    Adding 1-5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - Changin lower case letters
+ * @str: string of change
+ * Return: str
+ *
+ */
+char *string_toupper(char *str)
+{
+	int ind = 0;
+
+	while (str[ind])
+	{
+	if (str[ind] >= 'a' && str[ind] <= 'z')
+	str[ind] -= 32;
+	index++;
+	}
+	return (str);
+}
