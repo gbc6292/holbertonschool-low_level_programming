8ff2ce61d45188056daa9d939897a85236365ec7
gbc6292 <6292@holbertonstudents.com>
2023-03-05 03:36:29 +0000

    Adding 1-3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings.
+ * @s1: first string
+ * @s2: Second string
+ * Return: negative, positive or 0
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *si == *s2)
+	{
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+}
