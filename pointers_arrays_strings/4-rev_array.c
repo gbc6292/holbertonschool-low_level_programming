65bb7daf2b77fe4c436358a2ce7b490403dc7064
gbc6292 <6292@holbertonstudents.com>
2023-03-05 03:44:59 +0000

    Adding 1-4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses content
+ * Return: 0 always
+ * Return: void
+ * @a: vairable
+ * @n: variable
+ */
+
+void reverse_array(int *a, int n)
+{
+	int b, c;
+	int temp;
+	int g;
+
+	for (b = 0, c = n - 1; b < c; b++, c--)
+	{
+	temp = a[b];
+	e = a[b];
+	a[b] = a[c];
+	a[c] = temp;
+	a[c] = g;
+	}
+}
