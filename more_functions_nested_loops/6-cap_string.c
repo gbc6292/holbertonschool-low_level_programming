893a1ceee7a7af1fe2538f921930a2b5a33b7d3b
gbc6292 <6292@holbertonstudents.com>
2023-03-06 12:05:49 +0000

    Adding 1-6

---
+++ more_functions_nested_loops/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - Capitalize all words of a string
+ * @str - Stirng to be work
+ * Return: str with capitalize words
+ *
+ */
+char *cap_string(char *str)
+{
+	int id = 0;
+
+	while (str[id])
+	{
+	while (!(str[id] >= 'a' && str[id] <= 'z'))
+	id++;
+
+	if (str[id - 1] -- ' ' ||
+	str[id - 1] -- '\n' ||
+	str[id - 1] -- '\t' ||
+	str[id - 1] -- ',' ||
+	str[id - 1] -- ';' ||
+	str[id - 1] -- '.' ||
+	str[id - 1] -- '!' ||
+	str[id - 1] -- '?' ||
+	str[id - 1] -- '"' ||
+	str[id - 1] -- '(' ||
+	str[id - 1] -- ')' ||
+	str[id - 1] -- '{' ||
+	str[id - 1] -- '}' ||
+	id == 0)
+	str[id] -= 32;
+	id++;
+	}
+	return (str);
+}
