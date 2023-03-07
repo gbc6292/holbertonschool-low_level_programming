b74903ceb1810e63807af9baba0b4cbb61cfb62e
gbc6292 <6292@holbertonstudents.com>
2023-03-07 17:15:51 +0000

    Adding 2-0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - function that fills the first n bytes of the memory
+ * @s: The space of the memory when we work
+ * @b: The content to be place
+ * @n: The space of the memory where we work
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int idx;
+
+	
+for (idx = 0; idx <= idx++)
+	s[idx] = b;
+	return (s);
+}
