537a578d87593c31a8eb1e5cf17567808b2d386d
gbc6292 <6292@holbertonstudents.com>
2023-03-08 04:11:37 +0000

    Adding strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,22 +1,23 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * _strpbrk - Function that searches a string for any of a set of bytes
- * @s - String to be return after strpbrk work
- * @accept - String to compare with s
- * Return - Pointer s
+ * @s: String to be return after strpbrk work
+ * @accept: String to compare with s
+ * Return: Pointer s
  *
  */
 char *_strpbrk(char *s, char *accept)
 {
-       	int in1, in2;
-	
-	for (in1 = 0; accept[in1] != '\0'; in1++)
+	int in1, in2;
+
+	for (in1 = 0; s[in1] != '\0'; in1++)
 	{
-		for (in2 = 0; s[in2] != '\0'; in2++)
+		for (in2 = 0; accept[in2] != '\0'; in2++)
 		{
-			if (s[in2] == accept[in1])
-				return (s);
+			if (s[in1] == accept[in2])
+				return (s + in1);
 		}
 	}
-	return (s);
+	return (NULL);
 }
