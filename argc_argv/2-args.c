e7abfe9ff6016e8c9489f3f497920075bfb5d087
gbc6292 <6292@holbertonstudents.com>
2023-03-13 11:53:05 +0000

    Adding args 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Program that prints all artuments it receives
+ * @argc: Counter
+ * @argv: String that contain arguments to be printed
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int index;
+	
+	for (index = 0; index < argc; index++)
+		printf("%s\n", argv[index]);
+
+	return (0);
+}
