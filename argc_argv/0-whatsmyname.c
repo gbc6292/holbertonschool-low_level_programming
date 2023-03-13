cb268fcf48069072dff57689f909c80c98ac8433
gbc6292 <6292@holbertonstudents.com>
2023-03-13 11:22:10 +0000

    Adding Mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Function that print its name
+ * @argc: Counter of arguments
+ * @argv: Argument to be print
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
