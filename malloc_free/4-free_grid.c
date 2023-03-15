662829162652bc5a235a3dfc6cc4bdec3be93be2
gbc6292 <6292@holbertonstudents.com>
2023-03-15 03:32:58 +0000

    Adding free_grid

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - Function that free the allocated memomry 
+ * @grid: Allocated memory to be empty
+ * @height: String allocated in the memory
+ * Return: Void
+ *
+ */
+void free_grid(int **grid, int height)
+{
+	int idx;
+
+	for (idx = 0; idx < height; idx++)
+	{
+		free(grid[idx]);
+	}
+
+	free(grid);
+}
