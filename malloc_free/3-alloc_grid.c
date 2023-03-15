37fb8b3ccd2af4c920460aeeb9e99efa9fe43585
gbc6292 <6292@holbertonstudents.com>
2023-03-15 03:20:33 +0000

    Adding alloc_grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -24,8 +24,10 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i <= height; i++)
+	for (i = 0; i < height; i++)
 	{
+		a[i] = malloc(sizeof(int) * width);
+
 		if (a[i] == NULL)
 		{
 			for (j = i; j >= 0; j--)
