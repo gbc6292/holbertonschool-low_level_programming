ff73c6f54bbf07d7fb53a754150727b27de62271
gbc6292 <6292@holbertonstudents.com>
2023-03-16 22:39:35 +0000

    Adding malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - Function that allocate memory using malloc.
+ * @b: The space of memory to be allocate
+ * Return: void
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mal;
+	
+	mal = malloc(b);
+
+	if (mal == NULL)
+	{
+		exit(98);
+	}
+	return (mal);
+}
