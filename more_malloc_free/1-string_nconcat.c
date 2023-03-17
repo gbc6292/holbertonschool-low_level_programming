fe5c3e2c893d3ee11bbf579bcb0f08115e821625
gbc6292 <6292@holbertonstudents.com>
2023-03-17 12:51:59 +0000

    Adding sring_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,9 +18,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	l2 = strlen(s2);
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 
 	a = malloc(l1 + n + 1);
 
