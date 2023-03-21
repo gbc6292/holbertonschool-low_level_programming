5082bb09aaf804a8cf3525131c916346de690b59
gbc6292 <6292@holbertonstudents.com>
2023-03-21 11:58:32 +0000

    Adding array_range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * (max - min) + sizeof(int));
+	ptr = malloc(sizeof(int) * max + sizeof(int));
 	
 	if (ptr == NULL)
 		return (NULL);
