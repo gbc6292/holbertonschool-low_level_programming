bcb271a3fab049fd2ef3d8b9614029d3b5608875
gbc6292 <6292@holbertonstudents.com>
2023-04-11 09:45:05 +0000

    Adding diagsums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Function that diagonal numbers
+ * @a: number to be sum
+ * @size: quantity of numbers
+ * Return: 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, n, s1 = 0, s2 = 0;
+
+	for (i = o; i <= (size * size); i = i + size + 1)
+		s1 = s1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+		s2 = s2 + a[n];
+	printf("%d, %d\n", s1, s2);
+
