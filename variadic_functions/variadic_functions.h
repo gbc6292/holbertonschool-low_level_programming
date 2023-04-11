6e767ea5e87167bc863be9bf463f49f4f22ea392
gbc6292 <6292@holbertonstudents.com>
2023-04-11 10:48:12 +0000

    correcting .h double inclusion

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,6 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
@@ -8,3 +11,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+#endif
