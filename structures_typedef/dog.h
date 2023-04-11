ab1f20e6d8b4e0122130e4380c52590e7f3b847c
gbc6292 <6292@holbertonstudents.com>
2023-04-11 10:05:20 +0000

    fixing prototype name

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,21 +8,16 @@
  * @owner: dog owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for the dog structure
- */
-
-typedef struct dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
